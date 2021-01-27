171a3493bcafed9a12c8ec6f343d76a511437706
Md Emran Hossain <emran@medisys.com.sa>
2021-01-27 16:03:35 +0600

    Add new Force Update

---
+++ src/Components/ForceUpdate/ForceUpdate.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+
+class ForceUpdate extends Component{
+
+    constructor(){
+        super()
+        this.refresh=this.refresh.bind(this)
+    }
+
+    refresh(){
+        this.forceUpdate()
+    }
+
+    render(){
+        return(
+            <div>
+                <h6>{Math.random()}</h6>
+                <button className="btn btn-primary" onClick={this.refresh}>Refresh</button>
+            </div>
+        )
+    }
+}
+
+export default ForceUpdate
