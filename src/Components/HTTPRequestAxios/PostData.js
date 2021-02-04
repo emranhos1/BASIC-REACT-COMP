324129c9b2c56575c6b330a29d5d9b1954fad23f
Md Emran Hossain <emran@medisys.com.sa>
2021-02-04 17:42:07 +0600

    Create new Js woth axios for posting data

---
+++ src/Components/HTTPRequestAxios/PostData.js
@@ -0,0 +1,90 @@
+import React, { Component } from 'react';
+import Axios from "axios";
+
+class PostData extends Component {
+
+    constructor(){
+        super()
+        this.state={
+            nativeName   : '',
+            globalName   : '',
+            shortName    : '',
+            requestData  : [],
+            
+            // myData     : [],
+            // total      : 0,
+            // isSuccess  : false,
+            // message    :''
+        }
+    }
+
+    onChangeHandlerEvent = (event) =>{
+        var inputName   = event.target.name;
+        var inputValue  = event.target.value;
+
+        this.setState({
+            [inputName] : inputValue,
+            data   : [{[inputName] : [inputValue]}]
+        })
+
+        //Field Validation
+        var namePattern      = /^([a-zA-Z ]){2,30}$/;
+        var ShortnamePattern = /^([A-Z]){1,1}$/;
+
+        if (inputName === 'nativeName') {
+            if(!namePattern.test(inputValue)){
+                this.setState({nativeName : 'Native name invalid'})
+            }
+        }
+
+        if (inputName === 'globalName') {
+            if(!namePattern.test(inputValue)){
+                this.setState({globalName : 'Global name invalid'})
+            }
+        }
+
+        if (inputName === 'shortName') {
+            if(!ShortnamePattern.test(inputValue)){
+                this.setState({shortName : 'Short name invalid'})
+            }
+        }
+    }
+
+    onClickHandlerEvent = () =>{
+        // Axios.post('http://localhost:9191/v.1.0/rest/gender/add', this.state.nativeName)
+        //     .then(response=>{
+        //         console.log(response);
+        //         this.setState({
+        //             myData    : response.data.data,
+        //             total     : response.data.total,
+        //             isSuccess : response.data.success,
+        //             message   : response.data.message
+        //         })
+        //     })
+        //     .catch(error=>{
+        //         console.log(error);
+        //     })
+    }
+
+    render() {
+        return (
+            <div>
+                <h4>Post By API</h4>
+                {/* <form onSubmit={this.onSubmitHandlerEvent}> */}
+                    <input className='form-control' onChange={this.onChangeHandlerEvent} name='nativeName' type='text' placeholder='Native Name'></input>
+                    <h6>{this.state.nativeName}</h6>
+                    <br></br>
+                    <input className='form-control' onChange={this.onChangeHandlerEvent} name='globalName' type='text' placeholder='Global Name'></input>
+                    <h6>{this.state.globalName}</h6>
+                    <br></br>
+                    <input className='form-control' onChange={this.onChangeHandlerEvent} name='shortName'  type='text' placeholder='Short Name'></input>
+                    <h6>{this.state.shortName}</h6>
+                    <br></br>
+                    <input onClick={this.onClickHandlerEvent} className='btn btn-success' name='submit' type='submit' value='Submit'></input>
+                {/* </form> */}
+            </div>
+        );
+    }
+}
+
+export default PostData;
