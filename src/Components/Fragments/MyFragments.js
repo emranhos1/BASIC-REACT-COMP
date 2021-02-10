1651d9208ec6fde8cf0ca630480d69dda1c0f021
Md Emran Hossain <emran@medisys.com.sa>
2021-02-10 11:54:15 +0600

    Add Basic example for Fragments

---
+++ src/Components/Fragments/MyFragments.js
@@ -0,0 +1,17 @@
+import React, {Component, Fragment} from 'react';
+
+class MyFragments extends Component {
+    render() {
+        return (
+            <>
+                <h1>This is Fragment</h1>
+            </>
+            // or
+            // <Fragment>
+            //     <h1>This is Fragment</h1>
+            // </Fragment>
+        );
+    }
+}
+
+export default MyFragments;
